0cc212e2655782fd5b1d6dbdf728d53b5d1d1195
BenhurUFSM <84202210+BenhurUFSM@users.noreply.github.com>
2024-08-01 17:42:27 -0300

    teste de criação de rede

--- Trabalhos/t4/main.c
+++ Trabalhos/t4/main.c
@@ -9,6 +9,10 @@ int main(int argc, char *argv[])
   Rede rede = NULL;
   if (argc > 1) {
     rede = rede_cria(argv[1]);
+    if (rede == NULL) {
+      printf("problema na criação da rede do arquivo '%s'.\n", argv[1]);
+      exit(1);
+    }
   }
   int pontos = joga_com_tela(rede);
   printf("pontos: %d\n", pontos);
